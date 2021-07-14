27b1b627ef8969cb7a4e50e83602153d1e620f59
Adriaan de Jonge <adriaandejonge@gmail.com>
2021-07-14 14:05:06 +0800

    Create backend.go

---
+++ backend.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+        "log"
+        "net/http"
+        "time"
+)
+
+var slowdown time.Duration
+
+func main() {
+        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+                time.Sleep(slowdown)
+                w.Write([]byte(`{ "text": "Hello world!"}`))
+        })
+        http.HandleFunc("/slowdown", func(w http.ResponseWriter, r *http.Request) {
+                slowdown = 10000 * time.Millisecond
+                w.Write([]byte(`{ "text": "Slowed down server; it now takes 10s to respond"}`))
+        })
+        http.HandleFunc("/speedup", func(w http.ResponseWriter, r *http.Request) {
+                slowdown = 0 * time.Millisecond
+                w.Write([]byte(`{ "text": "Made server fast again; virtually no delay"}`))
+        })
+
+        if err := http.ListenAndServe(":8080", nil); err != nil {
+                slowdown = 0 * time.Millisecond
+                log.Fatal("ListenAndServe:", err)
+        }
+}
